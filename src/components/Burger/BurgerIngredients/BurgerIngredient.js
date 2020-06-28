d138ae8e387b8022bbf3601e689ad491dbf3e92a
pradeep misra <pradeep.misra@kognitiv.com>
2020-06-28 19:22:43 +0530

    Added dynamic ingredient component

--- src/components/Burger/BurgerIngredients/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredients/BurgerIngredient.js
@@ -8,16 +8,31 @@ const burgerIngredient = (props) => {
         case('bread-bottom'):
             ingredient = <div className={classes.BreadBottom}></div>
             break;
-        case('bread-bottom'):
-            ingredient = <div className={classes.BreadBottom}></div>
+        case('bread-top'):
+            ingredient = (
+            <div className={classes.BreadTop}>
+                <div className={classes.Seed1}></div>
+                <div className={classes.Seed2}></div>
+            </div>
+            )
             break;
-        case('bread-bottom'):
-            ingredient = <div className={classes.BreadBottom}></div>
+        case('meat'):
+            ingredient = <div className={classes.Meat}></div>
             break;
-        case('bread-bottom'):
-            ingredient = <div className={classes.BreadBottom}></div>
+        case('cheese'):
+            ingredient = <div className={classes.Cheese}></div>
             break;
+        case('salad'):
+            ingredient = <div className={classes.Salad}></div>
+            break;
+        case('bacon'):
+            ingredient = <div className={classes.Bacon}></div>
+            break;
+        default:
+            ingredient = null;
     }
+    return ingredient;
+
 };
 
 export default burgerIngredient;
